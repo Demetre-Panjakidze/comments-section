8771bd00ed43e432f9e58b6e93a7b6d02822dc20
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-01-20 16:23:57 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { default as data } from './src/data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'comments-section';
+  constructor() {
+    console.log(data);
+  }
 }
