3b7be712d2cf52d2359c9186ec3aa8e34ed67f27
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-01-21 18:44:05 +0400

    initial commit

--- src/app/comment-section/comment-section.component.ts
+++ src/app/comment-section/comment-section.component.ts
@@ -4,8 +4,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-comment-section',
   templateUrl: './comment-section.component.html',
-  styleUrls: ['./comment-section.component.scss']
+  styleUrls: ['./comment-section.component.scss'],
 })
 export class CommentSectionComponent {
   @Input() data: any;
+  @Input() comment: any;
+  @Input() index: any;
+
+  constructor() {
+    console.log(this.comment);
+  }
 }
