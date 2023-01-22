55b0af2d89b42248c94e9346d3cac8d301087857
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-01-22 18:41:59 +0400

    inital commit

--- src/app/comment-section/comment-section.component.ts
+++ src/app/comment-section/comment-section.component.ts
@@ -5,26 +5,48 @@ import { Input, OnInit, Component } from '@angular/core';
   templateUrl: './comment-section.component.html',
   styleUrls: ['./comment-section.component.scss'],
 })
-export class CommentSectionComponent implements OnInit{
+export class CommentSectionComponent implements OnInit {
   @Input() data: any;
   @Input() comment: any;
   @Input() index: number = -1;
-  main_user: string = "juliusomo";
+  main_user: string = 'juliusomo';
+  upvoteDisabled: boolean = false;
+  downvoteDisabled: boolean = false;
+  hasVoted: boolean = false;
+  hasDownvoted: boolean = false;
 
+  // LSD is localStorage data
   ngOnInit() {
     let LSD = localStorage.getItem('data');
     if (LSD) {
       this.data = JSON.parse(LSD);
     }
+    if (this.hasVoted) {
+      this.upvoteDisabled = true;
+    }
   }
 
-  upvote () {
-    this.data.comments[this.index].score++;
-    localStorage.setItem('data', JSON.stringify(this.data));
+  upvote() {
+    if (this.hasVoted) {
+      this.upvoteDisabled = true;
+      this.downvoteDisabled = false;
+    } else {
+      this.hasVoted = true;
+      this.hasDownvoted = false;
+      this.data.comments[this.index].score++;
+      localStorage.setItem('data', JSON.stringify(this.data));
+    }
   }
 
   downvote() {
-    this.data.comments[this.index].score--;
-    localStorage.setItem('data', JSON.stringify(this.data));
+    if (this.hasDownvoted) {
+      this.downvoteDisabled = true;
+      this.upvoteDisabled = false;
+    } else {
+      this.hasDownvoted = true;
+      this.hasVoted = false;
+      this.data.comments[this.index].score--;
+      localStorage.setItem('data', JSON.stringify(this.data));
+    }
   }
 }
